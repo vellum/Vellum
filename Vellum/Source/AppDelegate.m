ca52afbac271c0f42a3e15f6d3eee69679e5adef
David <david@velluminous.org>
2013-05-17 23:41:23 -0700

    Added google analytics integration

--- Vellum/Source/AppDelegate.m
+++ Vellum/Source/AppDelegate.m
@@ -1,6 +1,7 @@
 
 #import "AppDelegate.h"
 #import "VLMMainViewController.h"
+#import "GAI.h"
 
 @implementation AppDelegate
 @synthesize window;
@@ -17,6 +18,19 @@
     VLMMainViewController *vc = [[VLMMainViewController alloc] init];
     [self setMainViewController:vc];
     [window setRootViewController:vc];
+    
+    
+    // google
+    // Optional: automatically send uncaught exceptions to Google Analytics.
+    [GAI sharedInstance].trackUncaughtExceptions = YES;
+    // Optional: set debug to YES for extra debugging information.
+    [GAI sharedInstance].debug = YES;
+    /*
+    // Optional: set Google Analytics dispatch interval to e.g. 20 seconds.
+    [GAI sharedInstance].dispatchInterval = 20;
+    */
+    // Create tracker instance.
+    id<GAITracker> tracker = [[GAI sharedInstance] trackerWithTrackingId:@"UA-41031955-1"];
     
     return YES;
 }
