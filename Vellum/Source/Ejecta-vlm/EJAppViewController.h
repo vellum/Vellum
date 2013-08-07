fae9f964bcf8722178b60ab5fae168bc09668aa8
David <david@velluminous.org>
2013-08-07 15:14:24 -0700

    Folder cleanup: physical folder hierarchy now matches group hierarchy in project.

--- Vellum/Source/Ejecta-vlm/EJAppViewController.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface EJAppViewController : UIViewController {
-	BOOL landscapeMode;
-}
-
-- (void)callJS:(NSString *)statement;
-
-@end
--- Vellum/Source/Ejecta-vlm/EJUtils/EJBindingLifecycle.h
+++
@@ -1,9 +0,0 @@
-#import "EJBindingEventedBase.h"
-#import "EJJavaScriptView.h"
-
-@interface EJBindingLifecycle : EJBindingEventedBase <EJLifecycleDelegate>
-
-- (void)pause;
-- (void)resume;
-
-@end
